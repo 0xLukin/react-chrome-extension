32f22d39c9e294f300254bf25d39b27b721fbce8
0xXiaoChen <1255552127@qq.com>
2023-09-27 16:20:29 +0800

    fix

--- src/pages/Content/InjectedApp.jsx
+++ src/pages/Content/InjectedApp.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import '../../styles.css';
 
 export default function InjectedApp() {
-  return <div className=" bg-black">这里有一个AA钱包～</div>;
+  return <div className=" bg-black">这里有一个AA钱包～~</div>;
 }
